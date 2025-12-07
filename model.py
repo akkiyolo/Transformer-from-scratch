2bb0c3d908c4ced642778c91b98a748b9d40eafa
akkiyolo <akshatshukla069@gmail.com>
2025-12-08 00:33:08 +0530

    positional encoding layer

--- model.py
+++ model.py
@@ -3,33 +3,24 @@ import torch.nn as nn
 import math
 
 class InputEmbedding(nn.Module):
-    """
-    Converts token indices into dense embedding vectors and scales them.
-    This is the standard embedding layer used in the Transformer model
-    (Vaswani et al., 2017).
-    """
 
-    def __init__(self, d_model: int, vocab_size: int):
-        """
-        Parameters:
-        - d_model: Dimension of the embedding vectors (e.g., 512, 768)
-        - vocab_size: Total number of unique tokens in the vocabulary
-        """
-        super().__init__()
-        
-        self.d_model = d_model
-        self.vocab_size = vocab_size
+  def __init__(self,d_model:int, vocab_size:int):
+    super().__init__()
+    self.d_model=d_model
+    self.vocab_size=vocab_size
+    self.embedding=nn.Embedding(vocab_size,d_model)
 
-        # Lookup table that maps token IDs → embedding vectors of size d_model
-        self.embedding = nn.Embedding(vocab_size, d_model)
+  def forward(self,x):
+    return self.embedding(x)*math.sqrt(self.d_model)
 
-    def forward(self, x):
-        """
-        Forward pass:
-        - x: Tensor of token IDs (batch_size, sequence_length)
-        Returns:
-        - Embedded representation of shape (batch, seq_len, d_model)
-        - Scaled by sqrt(d_model), as described in Transformer paper
-        """
-        # Multiply by sqrt(d_model) to match positional encoding scale
-        return self.embedding(x) * math.sqrt(self.d_model)
+class PositionEncoding(nn.Module):
+
+  def __init__(self,d_model:int,seq_len:int,dropout:float):
+    super().__init__()
+    self.d_model=d_model
+    self.seq_len=seq_len
+    self.dropout=nn.Dropout(dropout)
+
+    #create a matrix of shape(seq_len and d_model)
+
+    pe=torch.zeros(seq_len,d_model)
